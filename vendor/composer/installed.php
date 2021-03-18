582ac2d1f14b0c441fe132d69df275f93f7de4db
[Kene Nwogu] <[nwogujoseph1111@gmail.com]>
2021-03-18 03:01:40 +0100

    set up composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9b67e5dcc4b950a315ceda2e4a3db792150811f2',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9b67e5dcc4b950a315ceda2e4a3db792150811f2',
+    ),
+  ),
+);
