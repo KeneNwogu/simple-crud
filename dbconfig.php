04e898a1e22c2254c8fd03f7e3f7de03500a22b7
[Kene Nwogu] <[nwogujoseph1111@gmail.com]>
2021-03-18 03:04:07 +0100

    set up composer

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 //Create a new SQLite3 Database
-$db = new PDO('sqlite:movies.sqlite');
+$db = new SQLite3('movies.db');
 
 //Create a new table to our database 
 $query = "CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, title TEXT,  category TEXT, rating INTEGER)";
