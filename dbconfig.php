ab8e78f325b928146ce185c165fe5de64ff92db6
[Kene Nwogu] <[nwogujoseph1111@gmail.com]>
2021-03-18 03:08:08 +0100

    commit

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 //Create a new SQLite3 Database
-$db = new PDO('movies.db');
+$db = new PDO('sqlite:movies.sqlite');
 
 //Create a new table to our database 
 $query = "CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, title TEXT,  category TEXT, rating INTEGER)";
